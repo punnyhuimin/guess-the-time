65a26273e51bf73e27f6d8b75ef8e97cb169bed7
declanleeler <declanlee-ler@hotmail.com>
2025-04-24 18:26:06 +0800

    Add docker

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// Default value for proxy target if not set in environment variables
+const proxyTarget = process.env.VITE_PROXY_TARGET || '127.0.0.1';
+
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true,
+    port: 5173,
+    open: '/',
+    proxy: {
+      '/api': {
+        target: `http://${proxyTarget}:8000`,
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+});
